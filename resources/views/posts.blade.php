e040f7e98ae65c78d18bc6e15a8a62f78b0539e7
giorgi295 <58777559+giorgi295@users.noreply.github.com>
2021-04-19 23:26:45 +0400

    DavalebaN2

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -13,18 +13,12 @@
             </a>
         </div>
 
-{{--        <div class="ml-4 text-lg leading-7 font-semibold">--}}
-{{--            <a href="{{route('delete', $post->id)}}" class="underline text-gray-900 dark:text-white">--}}
-{{--                <i class="fa fa-pencil-scuare">delete</i>--}}
-{{--            </a>--}}
-{{--        </div>--}}
-
         <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
             <div class="grid grid-cols-1 md:grid-cols-2">
                 <div class="p-6">
                     <div class="flex items-center">
                         <div class="ml-4 text-lg leading-7 font-semibold">
-                            <a href="#" class="underline text-gray-900 dark:text-white">
+                            <a href="{{route('post', $post->id)}}" class="underline text-gray-900 dark:text-white">
                                 {{$post->title}}
                             </a>
                         </div>
